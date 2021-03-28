56fb6ed47e705d8af9c305b385d0c6bb6e003c44
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-03-28 22:37:43 +0300

    avatar fix13

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -108,7 +108,7 @@ const updateProfile = (req, res, next) => {
 };
 
 const updateAvatar = (res, req, next) => {
-  const {avatar} = req.body;
+  const {avatar} = res.body;
   UserModel.findByIdAndUpdate(req.user._id, {$set: avatar}, {
     // runValidators: true,
     // new: true
