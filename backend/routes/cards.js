e8be6ad3c3958194b7ac81600ded95f64e61c6ac
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-03-28 20:25:48 +0300

    change validation

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const {
   getCards, postCard, deleteCard, putLike, deleteLike, getError,
 } = require('../controllers/cards');
-const { postCardValidator } = require('../middlewares/validators');
+// const { postCardValidator } = require('../middlewares/validators');
 
 
 router.get('/cards', getCards);
-router.post('/cards', postCardValidator, postCard);
+router.post('/cards', postCard);
 router.delete('/cards/:cardId', deleteCard);
 router.put('/cards/:cardId/likes', putLike);
 router.delete('/cards/:cardId/likes', deleteLike);
