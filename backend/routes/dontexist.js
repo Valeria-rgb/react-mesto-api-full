33e17125498f46b6aef39071b570af940d76ffd8
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-03-30 16:11:16 +0300

    fix dontExistRouter

--- backend/routes/dontexist.js
+++ backend/routes/dontexist.js
@@ -1,8 +1,7 @@
-const NotFoundError = require('../errors/not-found-err');
 const router = require('express').Router();
 
 router.all('*', (req, res) => {
-  throw new NotFoundError('Запрашиваемый ресурс не найден');
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 module.exports = router;
