cdd788139600416aadf7570f4860b2f11388674d
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-03-31 20:16:51 +0300

    fixing /*

--- backend/routes/dontexist.js
+++ backend/routes/dontexist.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const { NotFoundError } = require('../errors/not-found-err');
-
-router.all('*', () => {
-  NotFoundError('Запрашиваемый ресурс не найден');
-});
-
-module.exports = router;
+// const router = require('express').Router();
+// const { NotFoundError } = require('../errors/not-found-err');
+//
+// router.all('*', () => {
+//   NotFoundError('Запрашиваемый ресурс не найден');
+// });
+//
+// module.exports = router;
