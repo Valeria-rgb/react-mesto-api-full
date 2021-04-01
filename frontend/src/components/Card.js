834bb1c6313f37e468a3cbd8ae3f6fdf6b8e55b7
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-01 14:14:23 +0300

    id errsors

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,7 +5,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
     const currentUser = React.useContext(CurrentUserContext);
 
     const isOwn = card.owner === currentUser._id;
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
 
     const cardDeleteButtonClassName = (
         `card__trash ${isOwn ? '' : 'card__trash_invisible'}`
